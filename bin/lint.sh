29db223bf9ef1139356f12f74d576a59db9034a6
tatomyr <andrew.tatomyr@gmail.com>
2020-03-07 00:58:10 +0200

    v5: Merge lib/ folder to utils/; change sanitize algorithm

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-npx eslint\
-  ./core.js\
-  ./core.test.js\
-  ./utils/register-async.js\
-  ./utils/register-async.test.js\
-  ./lib/\
-  ./examples/
+npx eslint .
---
+++ utils/sanitize.js
@@ -0,0 +1,5 @@
+export const sanitize = input =>
+  input
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
