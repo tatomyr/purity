f960784a0d5ad0e326f6a21bdfbe393073843dc9
tatomyr <andrew.tatomyr@gmail.com>
2021-08-20 22:20:55 +0300

    v7: Add service worker for purity-todo-2 example to make it installable PWA

--- src/examples/purity-todo-2/index.ts
+++ src/examples/purity-todo-2/index.ts
@@ -2,3 +2,7 @@ import {mount} from './app.js'
 import {App} from './components/App.js'
 
 mount(App)
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+}
