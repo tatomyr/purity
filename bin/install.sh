be07be31a3dbf56611f200ebc81c0547b4ed3d5a
tatomyr <andrew.tatomyr@gmail.com>
2022-06-06 20:00:30 +0200

    chore: add github workflows

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Dependencies
-npm i ts-jest typescript jest minify eslint cypress@4.4.1 @typescript-eslint/eslint-plugin@latest @typescript-eslint/parser@latest eslint@latest
+# npm i ts-jest typescript jest minify eslint cypress@4.4.1 @typescript-eslint/eslint-plugin@latest @typescript-eslint/parser@latest eslint@latest
+npm i
 
 # Set up precommit hook
 ln -s ../../bin/pre-commit.sh .git/hooks/pre-commit
