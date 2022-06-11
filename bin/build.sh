926e4c157ccdacf346c7ee1f36f8d73e8f31c282
Andrew Tatomyr <andrew.tatomyr@gmail.com>
2022-06-11 14:38:02 +0300

    chore: add redirect from root to playground; adjust htmls (#32)

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash bin/pre-build.sh &&
+
 npx tsc &&
 find public/examples -iname '*.ts' -delete &&
 find public/playground -iname '*.ts' -delete &&
--- bin/pre-build.sh
+++ bin/pre-build.sh
@@ -4,3 +4,4 @@ rm -rf public/
 mkdir public/
 
 cp -r src/examples src/playground public
+cp src/index.html public
