02306383cf1435069ecf77453a8df94d6add082c
tatomyr <andrew.tatomyr@gmail.com>
2022-07-05 12:56:41 +0200

    fix: groomTasks on beforeunload

--- src/examples/purity-todo-2/services/startup.ts
+++ src/examples/purity-todo-2/services/startup.ts
@@ -1,15 +1,10 @@
-import {rerender, Task} from '../app.js'
 import {getJSON, saveJSON} from './storage.js'
-import {groomTasks, useTasks} from './tasks.js'
+import {groomTasks} from './tasks.js'
 
-// TODO: make use of it
-export const startup = async (): Promise<void> => {
-  // useTasks.fire({
-  //   mutation: async () => {
-  //     const tasks = await useTasks.unwrap()
-  //     await saveJSON({tasks: groomTasks(tasks)})
-  //   }
-  // })
-  const tasks = await useTasks.unwrap()
-  await saveJSON({tasks: groomTasks(tasks)})
+export const startup = (): void => {
+  window.onbeforeunload = e => {
+    getJSON({tasks: []})
+      .then(groomTasks)
+      .then(tasks => saveJSON({tasks: tasks}))
+  }
 }
