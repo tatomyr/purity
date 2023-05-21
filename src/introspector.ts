244195bf4dffa71e0f29a2f588e3ab5c61f13094
tatomyr <andrew.tatomyr@gmail.com>
2023-05-21 14:27:15 +0300

    chore: fix prettier

--- src/introspector.ts
+++ src/introspector.ts
@@ -26,26 +26,25 @@ export const makeIntrospector = (rerender: Rerender, isVisible = false) => {
 		})
 		const size = theState.get() ? 'calc(100% - 20px)' : '30px'
 		return render`
-		<pre id="the-introspector" ::click=${() => {
-			theState.set(s => !s)
-		}}>
-				${JSON.stringify(data, null, 2)}
-		</pre>
-		<style id="introspector-style">
-			#the-introspector {
-				position: fixed;
-				bottom: 10px;
-				right: 10px;
-				width: ${size};
-				height: ${size};
-				overflow: auto;
-				background: rgba(0, 0, 0, 0.75);
-				color: white;
-				z-index: 1000;
-				border: 1px solid red;
-			}
-			
-		</style>
-	`
+			<pre id="the-introspector" ::click=${() => {
+				theState.set(s => !s)
+			}}>
+					${JSON.stringify(data, null, 2)}
+			</pre>
+			<style id="introspector-style">
+				#the-introspector {
+					position: fixed;
+					bottom: 10px;
+					right: 10px;
+					width: ${size};
+					height: ${size};
+					overflow: auto;
+					background: rgba(0, 0, 0, 0.75);
+					color: white;
+					z-index: 1000;
+					border: 1px solid red;
+				}
+			</style>
+		`
 	}
 }
